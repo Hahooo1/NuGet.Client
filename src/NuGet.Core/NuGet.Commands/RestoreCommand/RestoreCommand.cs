442abc2d9d465ff79b3b6037ba617f65f029f150
Nikolche Kolev <nikolce1kolev@gmail.com>
2024-08-26 15:49:23 -0700

    Run the tests with pinning disabled

--- src/NuGet.Core/NuGet.Commands/RestoreCommand/RestoreCommand.cs
+++ src/NuGet.Core/NuGet.Commands/RestoreCommand/RestoreCommand.cs
@@ -146,7 +146,7 @@ namespace NuGet.Commands
             _success = !request.AdditionalMessages?.Any(m => m.Level == LogLevel.Error) ?? true;
 
             // Use the new resolver unless transitive pinning is disabled or the user has not explicitly opted out of using it
-            _enableNewDependencyResolver = !_request.Project.RestoreMetadata.UseLegacyDependencyResolver;
+            _enableNewDependencyResolver = !request.Project.RestoreMetadata.CentralPackageTransitivePinningEnabled && !_request.Project.RestoreMetadata.UseLegacyDependencyResolver;
         }
 
         public Task<RestoreResult> ExecuteAsync()
